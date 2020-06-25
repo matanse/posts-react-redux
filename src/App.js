a146bd8dd46033f67e726f3ead16e3fef4cf4a7c
Matan Selig <matanselig@MacBook-Pro-3.local>
2020-06-25 17:25:27 +0200

    port&dockerfile set

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Posts from "./components/Posts";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
